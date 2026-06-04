e3dcce943ed96bd0d38b78e7544470d38ec6cc4a
pille-w <p.weckesser@gmx.de>
2026-06-04 16:28:48 +0200

    Update register-form.php
    Probleme mit der privacy policy bei der Registrierung neuer User. Durch die Auskommentierung des privacy policy Blocks sollte es nun gehen. Wordpress sollte durch die neuen Versionen die Policyprüfung automatisch eingebaut haben.

--- register-form.php
+++ register-form.php
@@ -21,11 +21,11 @@
 			<!-- <p id="reg_passmail<?php $template->the_instance(); ?>"><?php echo apply_filters( 'tml_register_passmail_template_message', __( 'A password will be e-mailed to you.', 'theme-my-login' ) ); ?></p> -->
 		</div>
 		
-		<div class="form-row check-row column-row">
+		/*<div class="form-row check-row column-row">
 			<label for="privacy_data_police<?php $template->the_instance(); ?>"><?php _e( 'Accept the privacy data police', 'hhh-theme' ) ?></label>
 			<input type="checkbox" name="privacy_data_police" id="privacy_data_police<?php $template->the_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'privacy_data_police' ); ?>" />
 			<p>Please read our privacy data police carefully. <a href="https://tramprennen.org/privacy-data-policy/">You will find it here.</a> </p>
-		</div>
+		</div> */
 
 		<div class="form-row">
 			<input type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php esc_attr_e( 'Register', 'hhh-theme' ); ?>" />
